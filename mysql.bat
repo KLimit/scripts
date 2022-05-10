00498c98fa8f423eacd81674f90e46ceb2b62f6a
Henry Limm <henry.limm@motivps.com>
2022-05-10 10:46:04 -0700

    Add "macro" for mysqlsh.exe with parameters.

---
+++ mysql.bat
@@ -0,0 +1,8 @@
+﻿@echo off
+
+rem just run mysqlsh.exe with motiv params
+
+set uri=motivpsc_script@factory.motivps.com:3306
+set schema=motivpsc_production
+
+mysqlsh --uri="%uri%" -D %schema% --sql
