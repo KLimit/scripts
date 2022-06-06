32cefb052fcf8e0ea84c9f529c8b1ba62998fa00
Henry Limm <henry.limm@motivps.com>
2022-06-06 12:11:35 -0700

    Add extra on-entry python lines.

--- pcuio.bat
+++ pcuio.bat
@@ -4,6 +4,7 @@ git rev-parse --abbrev-ref HEAD | find /v "" | findstr /r /c:"^master$" > NUL & 
 	echo Switch to master branch and try again
 	break
 ) ELSE (
-	ECHO I am on master
+	ECHO I am on the main branch
 	ipython -i -c "import sys; import PCU_IO as pcuio; sys.argv = ['']; self = pcuio.TestControl(); self.helper.run_DUT_cmd('chassis output off'); self.helper.run_DUT_cmd('acc output off')"
 )
+exit
