0cabc279e624927c82f36b4e1c36b87f210e4571
Henry Limm <henry.limm@motivps.com>
2023-06-13 17:05:30 -0700

    Add show_missing feature.

--- ripgrep-and.py
+++ ripgrep-and.py
@@ -10,11 +10,18 @@ import subprocess
 RIPGREP = which('rg')
 
 
-def main(patterns, searchpath, for_ripgrep):
+def main(patterns, searchpath, show_missing, for_ripgrep):
     results = [rg(pattern, searchpath, for_ripgrep) for pattern in patterns]
-    if all(result == 0 for result in results):
-        return 0
-    return 1
+    exitcode = 0 if all(result == 0 for result in results) else 1
+    if show_missing:
+        failedpatterns = [
+            pattern for pattern, result
+            in zip(patterns, results)
+            if result != 0
+        ]
+        print('Did not match:')
+        print('\n'.join(failedpatterns))
+    return exitcode
 
 def rg(pattern, searchpath, for_ripgrep):
     """Do rg for one pattern on the searchpath."""
@@ -27,6 +34,11 @@ def mainargs(argv=None):
     pser = argparse.ArgumentParser(
         epilog=__doc__,
         allow_abbrev=False,
+    )
+    pser.add_argument(
+        '--show-missing',
+        help='print patterns that did not have a match, if any',
+        action='store_true',
     )
     pser.add_argument(
         'patterns',
@@ -45,4 +57,3 @@ def mainargs(argv=None):
 if __name__ == "__main__":
     import sys
     sys.exit(main(**mainargs()))
-
