9d6aa4cfb46bed638a5024b415bab60e2e7fb08a
Henry Limm <henry.limm@motivps.com>
2024-02-24 19:32:09 -0800

    Flesh out class implementation.

--- addtowallpapers.py
+++ addtowallpapers.py
@@ -3,6 +3,7 @@ import argparse
 from collections import namedtuple
 from dataclasses import dataclass
 import os
+import operator
 from pathlib import Path
 import re
 import sys
@@ -20,7 +21,8 @@ def get_dimensions(file_description):
     return Dimension(*map(int, match.groups()))
 
 
-def aspect_ratio(width, height):
+def aspect_ratio(file_description):
+    width, height = get_dimensions(file_description)
     return width/height
 
 
@@ -45,6 +47,8 @@ class WallpaperWatcher:
     verbose: bool = False
     recurse: bool = False
     force: bool = False
+    inclusive: bool = False
+
     def __post_init__(self):
         self.wallpaper_folder = Path(self.wallpaper_folder).absolute()
 
@@ -52,21 +56,45 @@ class WallpaperWatcher:
     def glob(self):
         return Path.rglob if self.recurse else Path.glob
 
-    def make_link_name(self, filename):
+    def make_link_path(self, filename):
         src = Path(filename).absolute()
         target = self.wallpaper_folder / src.name
         return target
 
-    def run(self, path):
-        file = Path(file).absolute()
-        files = [file]
-        if file.is_dir():
-            files = [
-                path for path in self.glob(file, '*')
-                if path.is_file()
-                and not path.is_relative_to(self.wallpaper_folder)
-            ]
+    def path_is_candidate(self, path):
+        """Return the nonexistent link's path or False."""
+        if path.is_dir():
+            # magic needs to open a file
+            return
+        description = magic.from_file(str(path))
+        link_path = self.make_link_path(path)
+        is_candidate = (
+            is_image(description)
+            and not path.is_relative_to(self.wallpaper_folder)
+            and (aspect := self.check_aspect(description))
+            and (not link_path.exists() or self.force)
+        )
+        if self.verbose and is_candidate:
+            verb = 'will link' if self.dry_run else 'linking'
+            # WARN: the name aspect will only exist if the is_candidate bool
+            # doesn't short-circuit before the assignment; however, the name is
+            # used if the is_candidate bool never short-circuits
+            print(f'{verb} {path} ({aspect})')
+        return is_candidate and link_path
 
+    def check_aspect(self, description):
+        """Check that the aspect ratio is in range and return False or the aspect."""
+        l = operator.le if self.inclusive else operator.lt
+        aspect = aspect_ratio(description)
+        return l(self.min_aspect, aspect) and l(aspect, self.max_aspect) and aspect
+
+    def run(self, path):
+        path = Path(path).absolute()
+        files = self.glob(path, '*') if path.is_dir() else [path]
+        for file in files:
+            if link_path := self.path_is_candidate(file) and not self.dry_run:
+                os.symlink(file, link_path)
+    main = run
 
 
 def main(
@@ -95,7 +123,7 @@ def main(
         description = magic.from_file(str(file))
         if not is_image(description):
             continue
-        aspect = aspect_ratio(*get_dimensions(description))
+        aspect = aspect_ratio(description)
         if min_aspect < aspect < max_aspect:
             # if (not target.exists() or force) and not dry_run:
             if not dry_run:
@@ -111,6 +139,7 @@ def mainargs(argv=None):
     pser.add_argument('wallpaper_folder')
     pser.add_argument('-a', '--aspect-min', type=float, default=1, dest='min_aspect')
     pser.add_argument('--aspect-max', type=float, default=999, dest='max_aspect')
+    pser.add_argument('--inclusive', action='store_true')
     pser.add_argument('-n', '--dry-run', action='store_true')
     pser.add_argument('-v', '--verbose', action='store_true')
     pser.add_argument('-r', '--recurse', action='store_true')
@@ -120,4 +149,7 @@ def mainargs(argv=None):
 
 
 if __name__ == "__main__":
-    sys.exit(main(**mainargs()))
+    # sys.exit(main(**mainargs()))
+    mainargs = mainargs()
+    path = mainargs.pop('file')
+    WallpaperWatcher(**mainargs).run(path)
