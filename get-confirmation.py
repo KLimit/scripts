6671346b00fed0a70d80d8d49a623f4ffca4e20d
Henry Limm <henry.limm@motivps.com>
2024-05-08 15:29:36 -0700

    Refactor get-confirmation into something more like a module.

--- get-confirmation.py
+++ get-confirmation.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
 import argparse
 import sys
-pser = argparse.ArgumentParser()
-pser.add_argument('title', nargs='?')
-pser.add_argument('query', nargs='?')
-pser.add_argument('-y', '--yes', action='store_true', help='return w/ exit code 0')
-pser.add_argument('-n', '--no', action='store_true', help='return w/ nonzero exit code (overrides --yes)')
-args = pser.parse_args()
-if args.no:
-    sys.exit(2)
-elif args.yes:
-    sys.exit(0)
-title = f'{args.title}:' if args.title else ''
-query = f' {args.query} ' if args.query else ''
-options = 'yN'
-prompt = f'{title}{query}({options}) > '
-# input_ = input
-# def input(prompt):
-#     print(prompt, end='', flush=True)
-#     input_()
-while (selection := input(prompt).casefold()) not in options.casefold():
-    pass
-if selection == 'y':
-    sys.exit(0)
-sys.exit(1)
+
+def get_confirmation(title, query, options='yN', prompt=' '):
+    # prompt = ' '.join([title, query, f'({options})', prompt])
+    fullprompt = f'{title}: ' if title else ''
+    if query:
+        fullprompt += 'query '
+    if options:
+        fullprompt += f'({options})'
+    prompt = fullprompt + prompt
+    while (selection := input(prompt).casefold()) not in options.casefold():
+        pass
+    return selection == 'y'
+
+if __name__ == '__main__':
+    pser = argparse.ArgumentParser()
+    pser.add_argument('title', nargs='?')
+    pser.add_argument('query', nargs='?')
+    pser.add_argument('-y', '--yes', action='store_true', help='return w/ exit code 0')
+    pser.add_argument('-n', '--no', action='store_true', help='return w/ nonzero exit code (overrides --yes)')
+    args = pser.parse_args()
+    if args.no:
+        sys.exit(2)
+    elif args.yes:
+        sys.exit(0)
+    if get_confirmation(args.title, args.query):
+        sys.exit(0)
+    sys.exit(1)
