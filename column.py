3672d075e897bbe3fdf6068ad6d21576047d5eac
Henry Limm <henry.limm@motivps.com>
2024-06-25 15:53:11 -0700

    Partial implementation of columns.

---
+++ column.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse
+import enum
+from os import get_terminal_size
+import sys
+
+
+Mode = enum.Enum('Mode', ['COLUMNS', 'ROWS', 'TABLE'])
+
+
+DEFAULT_COLUMNS = 80
+
+def main(file, columns=80, separator=' ', mode=Mode.COLUMNS):
+    if columns == 'auto':
+        try:
+            columns, _ = get_terminal_size(file)
+        except OSError:
+            columns = DEFAULT_COLUMNS
+    columns = int(columns)
+    lines = list(line.strip() for line in file)
+    longest = max(map(len, lines))
+    return 0
+
+
+def mainargs(argv=None):
+    pser = argparse.ArgumentParser()
+    a = pser.add_argument
+    a('file', type=argparse.FileType('r'), default='-')
+    a('-c', '--columns', default=DEFAULT_COLUMNS, help='output is formatted for a display `columns` wide ("auto" for terminal width)')
+    a('-s', '--separator', default=',  ', help='string used to separate columns',
+    a('-x', '--fillrows', dest='mode', const=Mode.ROWS, action='store_const')
+    args = pser.parse_args(argv)
+    return vars(args)
+
+if __name__ == "__main__":
+    sys.exit(main(**mainargs()))
