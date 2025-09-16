43375de8fc548a5ced383b683538e78e44c41097
Henry Limm <henry.limm@motivps.com>
2025-09-16 13:19:18 -0700

    (ruler) clip ruler to given length, add "stat" option

--- ruler.py
+++ ruler.py
@@ -6,16 +6,14 @@ def ruler(end=-1, interval=10, offset=0, tick='|', line='.'):
     if end < 0:
         end = os.get_terminal_size().columns
     end = end - offset
-    nticks = end % interval
+    # endticks = end % interval
     segment = f'{tick}{{n:{line}<{interval-1}}}'
     ruler_ = ''.join(
         segment.format(n=n)
         for n in [1, *range(interval, end, interval)]
     )
-    # TODO: add final marker if there's enough room, but not enough for a whole
-    # segment
-    # if end - len(ruler_)
-    return ruler_
+    # TODO: put marker at end with total width?
+    return ruler_[:end]
 
 def main(argv=None):
     import argparse
@@ -25,8 +23,12 @@ def main(argv=None):
     pser.add_argument('-o', '--offset', default=0, type=int)
     pser.add_argument('-t', '--tick', default='|')
     pser.add_argument('-l', '--line', default='.')
-    args = pser.parse_args(argv)
-    print(ruler(**vars(args)))
+    pser.add_argument('--stat', action='store_true')
+    args = vars(pser.parse_args(argv))
+    if args.pop('stat'):
+        print(os.get_terminal_size())
+    else:
+        print(ruler(**args))
 
 if __name__ == '__main__':
     # TODO: add command line interface
